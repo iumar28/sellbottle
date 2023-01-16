27c88266aaefcd1e5b8b1e11bcaf3833b8dc3e83
iumar28 <umar9897222@gmail.com>
2023-01-16 22:09:22 +0530

    bottles

---
+++ src/utils/data.js
@@ -0,0 +1,71 @@
+import I1 from "../img/dd.png" 
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Bottle1",
+    decp: "B&W",
+    price: "6.25",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Bottle2",
+    decp: "B&W",
+    price: "56",
+    imageSrc: I1,
+  },
+  {
+    id: 3, 
+    name: "Bottle3",
+    decp: "B&W",
+    price: "84",
+    imageSrc: I1,
+  },
+  {
+    id: 4,
+    name: "Bottle4",
+    decp: "B&W",
+    price: "79",
+    imageSrc: I1,
+  }
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Bottle1",
+    urlParamName: "bottle1",
+  },
+  {
+    id: 2,
+    name: "Bottle2",
+    urlParamName: "bottle2",
+  },
+  // {
+  //   id: 3,
+  //   name: "Rice",
+  //   urlParamName: "rice",
+  // },
+  // {
+  //   id: 4,
+  //   name: "Fish",
+  //   urlParamName: "fish",
+  // },
+  // {
+  //   id: 5,
+  //   name: "Fruits",
+  //   urlParamName: "fruits",
+  // },
+  // {
+  //   id: 6,
+  //   name: "Icecreams",
+  //   urlParamName: "icecreams",
+  // },
+
+  // {
+  //   id: 7,
+  //   name: "Soft Drinks",
+  //   urlParamName: "drinks",
+  // },
+];
